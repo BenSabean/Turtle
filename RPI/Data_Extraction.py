ce24ab43addb9de7ec4213265584a14f8ee0ed0a
ArthurBondar <arthur.bondar.1@gmail.com>
2018-02-20 20:38:12 -0400

    Feb 20, 2018. Data and Video program sketch ready

--- RPI/Data_Extraction.py
+++ RPI/Data_Extraction.py
@@ -4,19 +4,17 @@
     Data to be stored in a csv file, with date as the name
 '''
 
+import os
 from time import sleep
 import serial 
 
 
-''' METHODS & STRUCTURES '''
-# GPS Data containter
-class cGPS:
-    time: str
-    x: float
-    y: float
+''' ----- METHODS ----- '''
+# file path to store GPS data
+path = "/home/pi/Video/usb/"
 
 # Send request and receive one set of GPS Data
-def port_getGPS():
+def port_getGPS(port):
     port.write("GPS")               # Send GPS command
     message = ""
     message = port.readline()
@@ -26,7 +24,7 @@ def port_getGPS():
     return message
 
 # Send request and receive Date-Time
-def port_getTime():
+def port_getTime(port):
     port.write("TIME")               # Send GPS command
     message = ""
     message = port.readline()
@@ -35,8 +33,30 @@ def port_getTime():
         message = message[:-2]      # Clearing end of string garbage
     return message
 
+# Writes gps data into CSV File
+def store_GPS(_f, _time, _long, _lat):
+    _f.write(_time+","+_long+","+_lat+"\n")
 
-''' PARAMETERS AND VARIABLES'''
+# Checks wether new GPS file needs to be created (after 00:00)
+def newDay(dateTime, oldDay):
+    dateTime = dateTime.split('_')
+    day = dateTime[2]               # YYYY_MM_DD_HH_mm
+    if(int(day) > int(oldDay)):
+        return True
+    else:
+        return False
+    
+# Opens a file and prints headers if necessary
+def openFile(_path):
+    # if file is doesn't exist write headers and create file
+    if (os.path.isfile(_path) == False):
+        file f = open(file_name, "w")
+        csv1.write("TimeStamp,Latitude,Longitude\n")
+    else:
+        file f = open(file_name, "a")
+    return f
+
+''' ----- PARAMETERS & VARIABLES ----- '''
 # Setting parameters
 port = serial.Serial(
     port = "/dev/ttyS0",
@@ -44,27 +64,39 @@ port = serial.Serial(
     timeout=5,                      # in seconds
     parity = serial.PARITY_NONE,
     bytesize = serial.EIGHTBITS,
-    stopbits = serial.STOPBITS_ONE
+    stopbits = serial.STOPBITS_ONE 
 )
-# GPS Object
-GPS = cGPS("X", 0, 0)
 message = "start"
 
 
- ''' PROGRAM LOOP '''
+ ''' ----- PROGRAM SETUP ----- '''
 # Getting date from Adruino
-message = port_getTime()
-array = message.split()
-file_name = array[0] + "_" + array[1] + "_" + array[2] + ".csv"
-file_name = "/home/pi/Video/usb/" + file_name
-file _file = open(file_name, )
+message = port_getTime(port)
+date = message.split("_")
+# Creating file name for first GPS file
+file_name = path + date[0] + "_" + str(int(date[1])-1) + "_" + date[2] + ".csv"
+file csv1 = openFile(file_name)
+# Creating file name for second GPS file
+file_name = path + date[0] + "_" + date[1] + "_" + date[2] + ".csv"
+file csv2 = openFile(file_name)
 
+
+ ''' ----- PROGRAM LOOP ----- '''
 try:
     # loop untill the end of transmition
     while message != "EOT":
-
+        message = port_getGPS(port)
+        if(message != "" and message != "EOT"):
+            array = message.split(";")
+            # GPS data went over 00:00 -> new file needs to be open
+            if(newDay(array[0], str(int(date[1])-1)) == True):
+                # Store new GPS data
+                store_GPS(csv1, array[0], array[1], array[2])
+            else:
+                store_GPS(csv2, array[0], array[1], array[2])
 
 finally:
-
+    csv1.close()
+    csv2.close()
 
 
--- RPI/Video.py
+++ RPI/Video.py
@@ -1,42 +1,55 @@
+'''
+    This program records video and stores GPS data into a file
+    GPS data format is Timestamp, Longitude and Latitude
+    New File created for every video and GPS file
+'''
+
 import os
 from time import sleep
 from picamera
 import RPi.GPIO as GPIO
 import serial                   
 
-# Setting Serial port parameters
-port = serial.Serial(
-    port = "/dev/ttyS0",
-    baudrate=9600,
-    timeout=60,                    # port delay in seconds
-    parity = serial.PARITY_NONE,
-    bytesize = serial.EIGHTBITS,
-    stopbits = serial.STOPBITS_ONE
-)
 
-# gets a time string over Serial COM from Arduino module
-def getTime (port):
-    time = "2018-02-07-22-15"
-    port.write("TIME")              # Sending command to get Time
-    time = port.readline()
-    if (time != ""):
-        time = time[:-2]
-        print(time)                 # DEBUG
-        array = time.split('-')
-        # Converting to type string again with Zero fill
-        array = toInt(array)
-        array = toString(array)
-    return array
+''' ----- METHODS ----- '''
+# file path to store GPS data and Video
+path = "/home/pi/Video/usb/"
+
+# Send request and receive one set of GPS Data
+def port_getGPS(port):
+    port.write("GPS")               # Send GPS command
+    message = ""
+    message = port.readline()
+    if (message != "" and message != "EOT"):
+        port.write("OK")            # Handshake for data reception
+        message = message[:-2]      # Clearing end of string garbage
+    return message
+
+# Writes gps data into CSV File
+def store_GPS(_f, _time, _long, _lat):
+    _f.write(_time+","+_long+","+_lat+"\n")
+
+# Send request and receive Date-Time
+def port_getTime(port):
+    port.write("TIME")               # Send GPS command
+    message = ""
+    message = port.readline()
+    if (message != ""):
+        port.write("OK")            # Handshake for data reception
+        message = message[:-2]      # Clearing end of string garbage
+    return message
 
 # Converts string array to int array
 def toInt (array):
     for element in array:
         element = int(element)
+    return array
 
 # Converts int array to string array
 def toString (array):
     for element in array:
         element = str(element).zfill(2)
+    return array
 
 # keeping track of the time minute by minute
 def configureTime (_now):
@@ -62,15 +75,40 @@ def checkSleep (port):
             return True
     return False
 
-# indexes for date-time formating
+
+''' ----- PARAMETERS & VARIABLES ----- '''
+# Setting Serial parameters
+port = serial.Serial(
+    port = "/dev/ttyS0",
+    baudrate=9600,
+    timeout=1,                     # in seconds
+    parity = serial.PARITY_NONE,
+    bytesize = serial.EIGHTBITS,
+    stopbits = serial.STOPBITS_ONE
+)
+
+# Indexes for Date-Time
 year = 0
 month = 1
 day = 2
 hour = 3
 minute = 4
+# Getting date from Adruino
+message = port_getTime(port)
+# Format YYYY_MM_DD_HH_mm_SS
+now = message.split("_")
+# Variable to keep track of minutes
+prev_min = int(now[minute])
 
-# getting array with current data-time
-now = getTime(port)
+# Creating file name for GPS file
+file_name = path + now[year] + "_" + now[month] + "_" + now[day] + ".csv"
+# If file have not been created, writes the headers
+if (os.path.isfile(file_name) == False):
+    file csv = open(file_name, "w")
+    csv.write("TimeStamp,Latitude,Longitude\n")
+else:
+    file csv = open(file_name, "a")
+
 # creating camera object
 camera = picamera.PiCamera()
 try:
@@ -83,10 +121,22 @@ try:
     camera.annotate_background = picamera.Color('black')
     camera.annotate_text = now[year] + "/" + now[month] + "/" + now[day] + " " + now[hour] + ":" + now[minute]
     # Specifing output file
-    camera.start_recording('/home/pi/Desktop/' + now[year] + "_" + now[month] + "_" + now[day] + '.h264')
+    camera.start_recording(path + now[year] + "_" + now[month] + "_" + now[day] + '.h264')
+
+     ''' ----- PROGRAM LOOP ----- '''
     # Starting a loop and constantly checking for SLEEP command 
-    while checkSleep() != True:
+    while checkSleep() != True:     # 1 second delay from serial port waiting time
         camera.annotate_text = now[year] + "/" + now[month] + "/" + now[day] + " " + now[hour] + ":" + now[minute]
+        # Cheking if minute past
+        if(prev_min != int(now[minute])):
+            # Getting GPS position every min
+            message = port_getGPS(port)
+                if(message != "" and message != "EOT"):
+                    array = message.split(";")
+                    store_GPS(csv, array[0], array[1], array[2])
+            # Updating prev minute
+            prev_min = int(now[minute])
+        # Updating video time every second
         configureTime(now)
 
     # Finish recording
@@ -95,6 +145,7 @@ try:
     pass
 finally:
     camera.close()
+    csv.close()
     # os.system("shutdown -t now")
 
 
